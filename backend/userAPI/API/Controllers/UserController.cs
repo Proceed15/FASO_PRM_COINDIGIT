d4b5bf04b92af8fc0ca6a7cf875cbd6633ce9384
JOSE ROBERTO LISBOA DA SILVA FILHO <0031432412016@lti.fatecso.edu.br>
2025-03-07 18:04:11 -0300

    Início do Projeto com Swagger 13

--- backend/userAPI/API/Controllers/UserController.cs
+++ backend/userAPI/API/Controllers/UserController.cs
@@ -18,10 +18,36 @@ public class UserController : ControllerBase
         return Ok(result);
     }
 
+    [HttpGet]
+    public IActionResult getAllUsers()
+    {
+        var users = _userService.GetAllUsers();
+        return Ok(users);
+    }
+
     [HttpGet("{id}")]
     public IActionResult GetUserDetails(int id)
     {
         var user = _userService.GetUserDetails(id);
         return user != null ? Ok(user) : NotFound();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(int id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return NoContent();
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateUser(int id, User user)
+    {
+        if (id != user.Id)
+        {
+            return BadRequest();
+        }
+
+        await _userService.UpdateUserAsync(user);
+        return NoContent();
     }
 }
--- backend/userAPI/Infrastructure/Repositories/UserRepository.cs
+++ backend/userAPI/Infrastructure/Repositories/UserRepository.cs
@@ -14,4 +14,37 @@ public class UserRepository : IUserRepository
     }
 
     public User? GetById(int id) => _context.Users.Find(id);
+    
+    public List<User>? ListAll()
+    {
+        return _context.Users?.ToList() ?? new List<User>();
+    }
+
+    public async Task DeleteUserAsync(int userId)
+    {
+        var user = await _context.Users.FindAsync(userId);
+        if (user != null)
+        {
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task UpdateUserAsync(User user)
+    {
+        _context.Users.Update(user);
+        await _context.SaveChangesAsync();
+    }
+    public async Task EditUserAsync(User user)
+    {
+        var existingUser = await _context.Users.FindAsync(user.Id);
+        if (existingUser != null)
+        {
+            existingUser.Name = user.Name;
+            existingUser.Email = user.Email;
+            await _context.SaveChangesAsync();
+        }
+    }
+
+
 }
