781fa5a54cf3a2d5b91e6bb8336ac18b1e7c29f9
j <@gmail.com>
2025-03-14 17:51:34 -0300

    Updated Branch Develop

--- backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
+++ backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb9d952bac71671732e4df0b1bacc229fba80bb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62921fdea2ec54a24f0dde1beb98465a5559696e")]
 [assembly: System.Reflection.AssemblyProductAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
