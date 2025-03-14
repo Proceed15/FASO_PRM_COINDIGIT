22828d5867a6d96ed449a0b294cfd4eac37b0c75
felipe-salcedo <felipesalcedo,ramalho@gmail.com>
2025-03-14 17:42:46 -0300

    Alteração no Comentário no Arquivo appsettings.json

--- backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
+++ backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f414fa1215b8caf161b846e3938410c2cdfe1c9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deb9d952bac71671732e4df0b1bacc229fba80bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
