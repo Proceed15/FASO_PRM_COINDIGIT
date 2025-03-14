7f8efed14fe135d8c30332dfb3801cef7c93ff89
j <@gmail.com>
2025-03-13 21:01:22 -0300

    git file update

--- backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
+++ backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1d6197b6c93781b413d7f873f8e24bf8902f0be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f414fa1215b8caf161b846e3938410c2cdfe1c9e")]
 [assembly: System.Reflection.AssemblyProductAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
