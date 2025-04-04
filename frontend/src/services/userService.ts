307cf0d8a05928413c601e45731c4f3d7420591e
Isabel Almeida da Silva <isameidabebel@gmail.com>
2025-04-04 18:17:06 -0300

    User table CORS passed

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,3 +1,6 @@
+import { userAPI } from "./api";
+import axios from "axios";
+
 export interface User {
     Id?: number;
     Name: string;
@@ -8,27 +11,16 @@ export interface User {
     Photo: string;
 }
 
-const fakeUser: User[] = [{
-    Id: 1,
-    Name: "NomeUser",
-    Email: "User@gmail.com",
-    Phone: "(15)981088787",
-    Address: "Rua do usuário, 1",
-    Password: "senhaHashed",
-    Photo: "User.png"
-}, {
-    Id: 2,
-    Name: "TesteUser",
-    Email: "TesteUser@gmail.com",
-    Phone: "(11)40028922",
-    Address: "Rua do usuário, 2",
-    Password: "senhaHashed",
-    Photo: "User.png"
-}]
-
 const userService = {
-    async getUsers(): Promise<User[]> {
-        return fakeUser;
+    async getAll(): Promise<User[]> {
+        const header = {
+            Headers: {
+                'Accept': 'application/json',
+                'Access-Control-Allow-Origin': "*"
+            }
+        }
+        const response = await axios.get(userAPI.getAll(), header)
+        return response.data;
     }
 }
 
