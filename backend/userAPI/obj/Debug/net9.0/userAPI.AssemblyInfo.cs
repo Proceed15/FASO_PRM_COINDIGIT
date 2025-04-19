8316439c47eb168604a076ff96093d5e7b5e1908
Isameidabebel <isameidabebel@gmail.com>
2025-04-19 00:29:23 -0300

    Pacotes
    Next + DOTNET

--- backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
+++ backend/userAPI/obj/Debug/net9.0/userAPI.AssemblyInfo.cs
@@ -13,11 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111cdc1d89f6337a78f7bb8265de85e48a8a6b4d")]
-=======
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2111ebe52016869f67611f09365f2141523dece7")]
->>>>>>> release/1.2.0
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+844680f5511aba0b8524c8c9ca8d566d87b6518c")]
 [assembly: System.Reflection.AssemblyProductAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("userAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
