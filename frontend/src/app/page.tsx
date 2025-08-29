8556014f18f4cedc54fdec26c4eeac1ae796f4fa
Isabel Almeida da Silva <isameidabebel@gmail.com>
2025-08-29 17:24:37 -0300

    feat: Tela Inicial v2.0 Responsiva

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
       const distance = targetY - startY;
       let startTime: number | null = null;
 
-      const duration = 700; // mili-segundos
+      const duration = 700; // ml - mili segundos
 
       const animation = (currentTime: number) => {
         if (!startTime) startTime = currentTime;
@@ -52,47 +52,63 @@ export default function Home() {
 
       {/* PARTE DE CIMA */}
       <section
-        className="flex flex-col md:flex-row items-center justify-between 
-                   px-6 md:px-16 py-20 min-h-[95vh] relative"
+        className="flex flex-col items-center justify-between 
+       px-6 md:px-16 py-20 min-h-[95vh] relative"
         style={{
           backgroundImage: "url('/images/BACKGROUND-LOSANGOS.png')",
           backgroundSize: "cover",
           backgroundPosition: "center",
         }}
       >
-        {/* Textura do BG */}
-        <div className="absolute inset-0 bg-[rgba(48, 70, 160, 0.7)]"></div>
+        <style jsx>{`
+        @keyframes flush {
+          0% { background-position: -100% 0; }
+          50% { background-position: 100% 0; }
+          100% { background-position: -100% 0; }
+        }
+        .animate-flush {
+          background-size: 200% 100%;
+          animation: flush 15s linear infinite;
+        }
+      `}</style>
 
-        {/* Conteudo por cima do BG */}
-        <div className="relative z-10 flex flex-col md:flex-row items-center justify-between w-full gap-12">
-          {/* Logo + textos lado a lado */}
-          <div className="ml-[50px] mr-[50px] flex items-center gap-8">
+        {/*ANIMAÇÃO DE FLUSH*/}
+        <div className="bg-gradient-to-r from-[#265dbf]/30 via-white/20 to-[#265dbf]/30 
+    flex flex-col items-center justify-between 
+    px-6 md:px-16 py-20 min-h-[95vh] absolute top-0 left-0 w-full h-full z-10 animate-flush">
+        </div>
+
+
+        {/*CONTAINER TITULO + PERGUNTAS*/}
+        <div className="w-[100%] justify-center relative z-10 flex flex-col xl:flex-row items-center justify-between w-full gap-12">
+
+          {/*TITULO*/}
+          <div className="w-[100%] justify-center flex flex-col md:flex-row items-center gap-6 text-center md:text-left">
             <img
               src="/images/Logo_CoinDigit.png"
               alt="Logo CoinDigit"
-              className="w-40 h-40 md:w-48 md:h-48"
+              className="w-32 h-32 md:w-48 md:h-48"
             />
-            <div>
-              <h1 className="text-5xl md:text-6xl font-extrabold leading-tight">
+            <div className="max-w-[300px] md:max-w-[400px]">
+              <h1 className="text-4xl md:text-6xl font-extrabold leading-tight break-words">
                 COINDIGIT
               </h1>
-              <p className="ml-[10px] text-xl md:text-2xl text-gray-200 font-medium mt-2">
+              <p className="text-lg md:text-2xl text-gray-200 font-medium mt-2">
                 Invista no seu Futuro!
               </p>
             </div>
           </div>
 
-          {/* Perguntas */}
-          <div className="ml-[30px] mr-[30px] flex flex-col gap-4 w-full md:w-1/2">
+          {/*PERGUNTAS*/}
+          <div className="w-[100%] justify-center flex flex-col gap-4 w-full xl:w-full">
             {perguntas.map((q) => (
               <button
                 key={q.id}
                 onClick={() => scrollToSection(q.id)}
                 className="mt-[15px] bg-[#0e1a52] rounded-lg px-6 py-5 
-                           shadow-md border border-white 
-                           hover:border--400 hover:bg-[#162a6b] 
-                           transition-all duration-300 text-lg md:text-xl 
-                           text-center md:text-left font-medium"
+               shadow-md border border-white 
+               hover:bg-[#162a6b] transition-all duration-300 
+               text-lg md:text-xl text-left md:text-left font-medium"
               >
                 {q.texto}
               </button>
@@ -104,8 +120,7 @@ export default function Home() {
       {/* PARTE DE BAIXO */}
       <section className="px-6 md:px-16 py-20 space-y-20 bg-gradient-to-r from-[#283976] to-[#101d4b]">
         {/* "Container" das IMG */}
-        <style jsx>{`
-        
+        <style jsx>{`      
           .img-box {
           width: 100%;
           max-width: 500px;
@@ -126,12 +141,12 @@ export default function Home() {
         {/* Pergunta 1 */}
         <div
           id="p1"
-          className="flex flex-col md:flex-row items-center justify-between gap-16"
+          className="flex flex-col xl:flex-row items-center justify-between gap-16"
         >
-          <div className="img-box ml-[70px] mr-[70px]">
+          <div className="img-box">
             <img src="/images/O-QUE-E-CRIPTOMOEDA.jpg" alt="Criptomoedas" />
           </div>
-          <p className="ml-[50px] mr-[50px] text-2xl md:text-2xl text-gray-200 text-justify max-w-2xl">
+          <p className="text-2xl xl:text-2xl text-gray-200 text-justify max-w-2xl">
             A criptomoeda refere-se a qualquer forma de moeda que existe digital
             ou virtualmente e usa criptografia para garantir a realização de
             transações. As criptomoedas não têm uma autoridade central de
@@ -143,12 +158,12 @@ export default function Home() {
         {/* Pergunta 2 */}
         <div
           id="p2"
-          className="flex flex-col md:flex-row-reverse items-center justify-between gap-16"
+          className="flex flex-col xl:flex-row-reverse items-center justify-between gap-16"
         >
-          <div className="img-box ml-[70px] mr-[70px]">
+          <div className="img-box">
             <img src="/images/O-QUE-E-COTACAO.jpg" alt="Cotação reversa" />
           </div>
-          <p className="ml-[50px] mr-[50px] text-2xl md:text-2xl text-gray-200 text-justify max-w-2xl">
+          <p className="text-2xl xl:text-2xl text-gray-200 text-justify max-w-2xl">
             Cotação reversa em criptomoedas é quando se inverte a forma
             tradicional de mostrar o valor entre duas moedas. Em vez de dizer
             "1 Bitcoin = 60.000 USD", a cotação reversa mostra "1 USD = 0,00001667
@@ -160,12 +175,12 @@ export default function Home() {
         {/* Pergunta 3 */}
         <div
           id="p3"
-          className="flex flex-col md:flex-row items-center justify-between gap-16"
+          className="flex flex-col xl:flex-row items-center justify-between gap-16"
         >
-          <div className="img-box ml-[70px] mr-[70px]">
+          <div className="img-box">
             <img src="/images/O-QUE-E-LASTRO.jpg" alt="Lastro" />
           </div>
-          <p className="ml-[50px] mr-[50px] text-2xl md:text-2xl text-gray-200 text-justify max-w-2xl">
+          <p className="text-2xl xl:text-2xl text-gray-200 text-justify max-w-2xl">
             O lastro de uma moeda significa o valor real que a sustenta. No caso
             das criptomoedas, muitas não possuem lastro físico (como ouro ou
             dólar), mas sim a confiança dos usuários e a tecnologia blockchain
@@ -176,12 +191,12 @@ export default function Home() {
         {/* Pergunta 4 */}
         <div
           id="p4"
-          className="flex flex-col md:flex-row-reverse items-center justify-between gap-16"
+          className="flex flex-col xl:flex-row-reverse items-center justify-between gap-16"
         >
-          <div className="img-box ml-[70px] mr-[70px]">
+          <div className="img-box">
             <img src="/images/INVESTIR-SEGURO.jpg" alt="Investimento seguro" />
           </div>
-          <p className="ml-[50px] mr-[50px] text-2xl md:text-2xl text-gray-200 text-justify max-w-2xl">
+          <p className="text-2xl xl:text-2xl text-gray-200 text-justify max-w-2xl">
             Investir em criptomoedas pode ser seguro se feito com estudo e
             cautela. Apesar de voláteis, elas oferecem oportunidades de altos
             ganhos, mas também riscos. Diversificar e entender o mercado é
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-// tailwind.config.js
+//mudar base tailwind
 module.exports = {
+
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl2: "1200px",
+      xl: "1200px",//menor que 1200px execute
+      "2xl": "1536px",
+    },
+
     extend: {
       animation: {
         gradientMove: "gradientMove 7s ease infinite",
@@ -16,6 +27,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
 }
+
 
