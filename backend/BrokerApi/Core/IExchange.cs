9a918a1b74d28d05af2e8abc8afc19c3dcfb98dc
FelipeSalcedo <felipesalcedo.ramalho@gmail.com>
2025-09-19 17:35:16 -0300

    feito ate metade da pasta infra
    Felipe

---
+++ backend/BrokerApi/Core/IExchange.cs
@@ -0,0 +1,12 @@
+namespace BrokerApi.API.Core
+{
+    public interface IExchange
+    {
+        string Name { get; }
+        ExchangeType Type { get; }
+
+        void BindQueue(string routingKey, IQueue queue);
+        void UnbindQueue(string routingKey, IQueue queue);
+        Task PublishAsync(Message message);
+    }
+}
